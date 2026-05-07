3ffb906f30b649ba75a3033d1fa82aaec4641e28
Lucas_Arjona <celarjmun@alum.es>
2026-05-07 11:43:28 +0200

    añadido el codigo del ESP secundario encargado del streaming de video y los displays de la bateria

---
+++ video_streaming/CameraWebServer/CameraWebServer.ino
@@ -0,0 +1,218 @@
+#include <Arduino.h>
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "board_config.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+unsigned long Vm_startMillis = 0;  //some global variables available anywhere in the program
+unsigned long Vm_currentMillis = 0;
+const unsigned long Vm_period = 1000;
+
+const int analogPin = 17;
+const float dividerRatio = 0.244; // Your measured ratio
+const float refVoltage = 3.3;     // Measure your 3V3 pin and update this for 100% accuracy
+
+
+
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+//const char *ssid = "iPhone de Lucaasduck ";
+//const char *password = "BELGICA931";
+
+const char *ssid = "Battlebot_CAM";
+const char *password = "12345678";
+
+void startCameraServer();
+void setupLedFlash();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  Serial.println("Iniciando programa...");
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_QVGA;//  QVGA
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
+
+// Setup LED FLash if LED pin is defined in camera_pins.h
+#if defined(LED_GPIO_NUM)
+  setupLedFlash();
+#endif  
+  WiFi.mode(WIFI_STA);
+  IPAddress local_IP(192, 168, 4, 2);
+  IPAddress gateway(192, 168, 4, 1);
+  IPAddress subnet(255, 255, 255, 0);
+
+if (!WiFi.config(local_IP, gateway, subnet)) {
+  Serial.println("Error configurando IP estatica");
+}
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  Serial.print("WiFi connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP()); //http://192.168.4.2
+  Serial.println("' to connect");
+
+  // --- incializacion pantalla 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.setRotation(2);
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Battery Monitor");
+  display.display();
+  delay(1000);
+
+}
+
+void loop() {
+  // ---- lectura de bateria y mostrar en displays ------
+  Vm_currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+  if (Vm_currentMillis - Vm_startMillis >= Vm_period)  //test whether the period has elapsed
+  {
+
+   Vm_startMillis = millis();
+  // 1. Calculate Voltage
+  int rawValue = analogRead(analogPin);
+  float pinVoltage = (rawValue / 4095.0) * refVoltage;
+  float batteryVoltage = pinVoltage / dividerRatio;
+
+  // 2. Update Display
+  display.clearDisplay();
+  
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("LIPO VOLTAGE:");
+
+  display.setTextSize(2);
+  display.setCursor(0, 15);
+
+  // --- Leading Zero Logic ---
+  if (batteryVoltage < 10.0) {
+    display.print("0"); // Add the "padding" zero
+  }
+  
+  if (batteryVoltage > 0.1){
+  display.print(batteryVoltage, 2); 
+  display.print(" V");}
+  else {
+    display.print("0.00 V");
+  }
+
+  // --- Low Battery Warning ---
+  if (batteryVoltage < 11.0) {
+    display.setTextSize(2);     // Switch to small text so it fits on the side
+    display.print("!!!"); 
+    display.invertDisplay(true);     // Add the warning signs
+  }
+  else {
+    display.invertDisplay(false);
+  }
+  display.display();
+  
+  }
+}
